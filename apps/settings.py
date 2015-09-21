4e48624233cdcb3d68a38a9a7aa5117a310af4a3
Your Name <you@example.com>
2015-09-21 01:03:23 -0400

    update settings ... ff

--- apps/settings.py
+++ apps/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')=febzrz@+^z69=twoq33w#b7(srs&c26a(aqixv8l)d=r@+(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
