76c0bf0a53c4b6b249d2397c773646ed76f32218
Your Name <you@example.com>
2015-09-21 22:09:23 -0400

    Update Views

--- books_cbv/views.py
+++ books_cbv/views.py
@@ -4,9 +4,19 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 
 from books_cbv.models import Book
+# views.py
+import logging
+logger = logging.getLogger(__name__)
+
 
 class BookList(ListView):
     model = Book
+
+    def get_context_data(self, **kwargs):
+        logger.debug("this is a debug message! ......................")
+        cont = super(BookList,self).get_context_data(**kwargs)
+        cont["data"] = "amira"
+        return cont
 
 class BookCreate(CreateView):
     model = Book
@@ -21,3 +31,10 @@ class BookUpdate(UpdateView):
 class BookDelete(DeleteView):
     model = Book
     success_url = reverse_lazy('books_cbv:book_list')
+
+
+def myfunction():
+    logger.debug("this is a debug message!")
+
+def myotherfunction():
+    logger.error("this is an error message!!")
